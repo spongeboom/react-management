79a44ddde2aad2d6bca7297879b80be98243fd67
spongeboom <zlzlwl12@naver.com>
2019-05-24 17:34:26 +0900

    implement Customer Add module , mysql (server) connect

--- client/management/src/App.js
+++ client/management/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Customer from './components/Customer';
+import CustomerAdd from './components/CustomerAdd';
 import Paper from '@material-ui/core/Paper';
 import Table from '@material-ui/core/Table';
 import TableHead from '@material-ui/core/TableHead';
@@ -26,9 +27,22 @@ const styles = theme => ({
 
 class App extends React.Component {
 
-  state = {
-    customers: "",
-    completed: 0
+  constructor(props){
+    super(props);
+    this.state = {
+      customers: '',
+      completed: 0
+    }
+  }
+
+  stateRefresh = () => {
+    this.setState({
+      customers: '',
+      completed: 0
+    })
+    this.callApi()
+      .then(res => this.setState({customers: res}))
+      .catch(err => console.log(err));
   }
 
   progress = () => {
@@ -52,6 +66,7 @@ class App extends React.Component {
   render(){
     const { classes } =this.props;
     return(
+      <div>
       <Paper className={ classes.root}>
         <Table className ={classes.table}>
           <TableHead>
@@ -75,6 +90,8 @@ class App extends React.Component {
           </TableBody>
         </Table>
       </Paper>
+      <CustomerAdd stateRefresh={this.stateRefresh}/>
+      </div>
     );
   }
 }
--- server.js
+++ server.js
@@ -2,37 +2,52 @@ const express =require('express')
     , bodyParser =require('body-parser')
     , app = express()
     , port = process.env.PORT || 4000
+    , fs = require('fs')
+    , mysql = require('mysql')
+    , data = fs.readFileSync('./database.json')
+    , conf = JSON.parse(data)
+    , multer = require('multer')
+    , upload = multer({dest: './upload'})
 
-  app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+      conn = mysql.createConnection({
+        host:conf.host,
+        user:conf.user,
+        password:conf.password,
+        port:conf.port,
+        database:conf.database
+      })
+  conn.connect();
 
   app.get('/api/customers', (req, res) => {
-    res.send([
-      {
-      'id':1,
-      'image':'https://placeimg.com/64/64/1',
-      'name' : '김성윤',
-      'birthday' : '960424',
-      'gender' :'남자',
-      'job':'학생'
-      },
-      {
-      'id':2,
-      'image':'https://placeimg.com/64/64/2',
-      'name' : '홍길동',
-      'birthday' : '960412',
-      'gender' :'남자',
-      'job':'학생'
-      },
-      {
-      'id':3,
-      'image':'https://placeimg.com/64/64/3',
-      'name' : '홍길순',
-      'birthday' : '960404',
-      'gender' :'남자',
-      'job':'학생'
-      }
-    ])
+    conn.query("SELECT * FROM CUSTOMER", (err,rows, fields) => {
+      if(err) throw console.log(err);
+      res.send(rows);
+    })
+  })
+
+
+  app.post('/api/test', (req,res) => {
+    console.log(req.body.birthday);
+    res.send(req.body.test);
+  })
+
+  app.use('/image',express.static('./upload'));
+  app.post('/api/customers',upload.single('image'),(req,res) =>{
+    let sql = 'INSERT INTO CUSTOMER VALUES (null,?,?,?,?,?)'
+      , image = '/image/' + req.file.filename
+      , name = req.body.name
+      , birthday = req.body.birthday
+      , gender = req.body.gender
+      , job = req.body.job
+      , params = [image,name,birthday,gender,job];
+
+      conn.query(sql,params, (err,rows,fields) => {
+        if(err) throw err
+        res.send(rows);
+      })
   })
 
   app.listen(port, () => {
