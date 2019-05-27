8228bc76c404650c7e88771e48f9b921785ed526
spongeboom <zlzlwl12@naver.com>
2019-05-27 18:58:06 +0900

    Update UI/UX & Add Customer & Search Module

--- client/management/src/index.js
+++ client/management/src/index.js
@@ -2,9 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const theme = createMuiTheme({
+  typography : {
+    fontFamily: '"Noto Sans KR", serif',
+  }
+})
+
+ReactDOM.render(<MuiThemeProvider theme={theme}> <App /> </MuiThemeProvider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
